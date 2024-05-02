c0c4cd5cb6026a7014c80860a11fdb5dc21ce7a2
SimonAllenOliver <simonallenoliver@gmail.com>
2024-05-02 17:55:05 -0500

    added tile map

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // reference to the Rigidbody2D component attached to the player object. Rigidbody2D allows us to apply physics-based movement.
+    public Rigidbody2D rb;
+
+    // determines the player’s movement speed
+    public float moveSpeed = 5f;
+
+    // local variable stores the horizontal input value (left or right movement)
+    float horizontalMovement;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +22,13 @@ public class PlayerMovement : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    // sets the player’s velocity based on the horizontal input (horizontalMovement) and the defined moveSpeed
     {
-        
+        rb.velocity = new Vector2(horizontalMovement * moveSpeed, rb.velocity.y);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontalMovement = context.ReadValue<Vector2>().x;
     }
 }
